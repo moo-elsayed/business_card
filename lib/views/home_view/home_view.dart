ee0a9ad76d13247bc0f93fe6e574a40844922995
kk28xd <159706787+kk28xd@users.noreply.github.com>
2024-12-04 03:46:02 +0200

    change background color

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -8,6 +8,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: const Color(0xffFEC107),
         centerTitle: true,
